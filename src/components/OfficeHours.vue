a152667dc0bd1b6ba6dde2059abc7947a760f98d
Roland <roland.paire@gmx.fr>
2023-05-07 16:55:40 +0200

    Fix: open hours

--- src/components/OfficeHours.vue
+++ src/components/OfficeHours.vue
@@ -2,15 +2,15 @@
   <v-card>
     <v-card-title class="text-center">Horaires de consultation</v-card-title>
     <v-card-text>
-      <v-list>
-        <v-list-item v-for="(value, key, index) in businessHours" :key="index">
-          <v-row>
-            <v-col>{{ DayOfWeek[parseInt(key)] }}</v-col>
-            <v-col>{{ formatBusinessHours(value, true) }}</v-col>
-            <v-col>{{ formatBusinessHours(value, false) }}</v-col>
-          </v-row>
-        </v-list-item>
-      </v-list>
+      <v-table density="compact" hover class="custom-table">
+        <tbody>
+          <tr v-for="(value, key, index) in businessHours" :key="index">
+            <td>{{ DayOfWeek[parseInt(key)] }}</td>
+            <td>{{ formatBusinessHours(value, true) }}</td>
+            <td>{{ formatBusinessHours(value, false) }}</td>
+          </tr>
+        </tbody>
+      </v-table>
     </v-card-text>
   </v-card>
 </template>
@@ -18,13 +18,13 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 enum DayOfWeek {
-  Sunday = 0,
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
+  Dimanche = 0,
+  Lundi,
+  Mardi,
+  Mercredi,
+  Jeudi,
+  Vendredi,
+  Samedi,
 }
 interface IHours {
   morningOpen: string;
@@ -38,25 +38,25 @@ type BusinessHours = {
 };
 
 const businessHours = ref<BusinessHours>({
-  [DayOfWeek.Monday]: {
+  [DayOfWeek.Lundi]: {
     morningOpen: '9h',
     morningClose: '12h',
     afternoonOpen: '13h',
     afternoonClose: '18h',
   },
-  [DayOfWeek.Tuesday]: {
+  [DayOfWeek.Mardi]: {
     morningOpen: '9h',
     morningClose: '12h',
     afternoonOpen: '13h30',
     afternoonClose: '18h30',
   },
-  [DayOfWeek.Thursday]: {
+  [DayOfWeek.Jeudi]: {
     morningOpen: '9h',
     morningClose: '12h',
     afternoonOpen: '13h30',
     afternoonClose: '17h30',
   },
-  [DayOfWeek.Friday]: {
+  [DayOfWeek.Vendredi]: {
     morningOpen: '9h',
     morningClose: '12h',
     afternoonOpen: '13h',
@@ -80,4 +80,8 @@ function formatBusinessHours(hours: IHours, isMorning: boolean): string {
 onMounted(() => console.log(businessHours.value));
 </script>
 
-<style scoped></style>
+<style scoped>
+.custom-table > .v-table__wrapper > table > tbody > tr:not(:last-child) > td {
+  border-bottom: none;
+}
+</style>
