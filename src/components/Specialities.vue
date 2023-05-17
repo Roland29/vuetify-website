b25982180f27da4d5e8ed1315dddc228253b5077
Roland <roland.paire@gmx.fr>
2023-05-17 16:55:51 +0200

    fix: typo

--- src/components/Specialities.vue
+++ src/components/Specialities.vue
@@ -5,11 +5,11 @@
     <v-card-title class="text-center">Omnipratique et Parodontie</v-card-title>
     <v-card-text class="text-body-1">
       <v-container>
-        Notre cabinet dentaire s'attache à préserver la santé bucco-dentaire, à rétablir ou améliorer le sourrire.<br /><br />
-        Il s'oriente vers le traitement des maladies parondotales.<br />
+        Notre cabinet dentaire s'attache à préserver la santé bucco-dentaire, à rétablir ou améliorer le sourire.<br /><br />
+        Il s'oriente vers le traitement des maladies parodontales.<br />
         <span class="text-body-2">
           (Les maladies des gencives et du parodonte. Elles sont plus connues sous le nom de déchaussement des dents et
-          conduisent à la perte des dents.)
+          conduisent à la perte des dents)
         </span>
       </v-container>
     </v-card-text>
